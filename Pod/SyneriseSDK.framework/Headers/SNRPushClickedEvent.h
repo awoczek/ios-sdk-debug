72e9202031248966f11bec9df60fa856d7718efa
Gitlab CI/CD <krzysztof.kurzawa@synerise.com>
2019-03-26 10:44:38 +0100

    Deploy 1.0.4 version

---
+++ Pod/SyneriseSDK.framework/Headers/SNRPushClickedEvent.h
@@ -0,0 +1,24 @@
+//
+//  SNRPushClickedEvent.h
+//  SyneriseSDK
+//
+//  Created by Synerise
+//  Copyright © 2018 Synerise. All rights reserved.
+//
+
+#import "SNREvent.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+/**
+ * @class SNRPushClickedEvent
+ *
+ * Represents a 'client clicked push' event. It is used for push message interaction tracking.
+ */
+
+NS_SWIFT_NAME(PushClickedEvent)
+@interface SNRPushClickedEvent : SNREvent
+
+@end
+
+NS_ASSUME_NONNULL_END

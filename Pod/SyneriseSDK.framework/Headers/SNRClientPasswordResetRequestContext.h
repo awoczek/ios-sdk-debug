b44b698bdcdebbe3166515a64ba4d272c23e02be
Gitlab CI/CD <krzysztof.kurzawa@synerise.com>
2019-03-28 10:55:29 +0100

    Update of version 1.0.30

---
+++ Pod/SyneriseSDK.framework/Headers/SNRClientPasswordResetRequestContext.h
@@ -0,0 +1,29 @@
+//
+//  SNRClientPasswordResetRequestContext.h
+//  SyneriseSDK
+//
+//  Created by Synerise
+//  Copyright (c) 2018 Synerise. All rights reserved.
+//
+
+#import "SNRBaseModel.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+/**
+ * @class SNRClientPasswordResetRequestContext
+ */
+
+NS_SWIFT_NAME(ClientPasswordResetRequestContext)
+@interface SNRClientPasswordResetRequestContext : SNRBaseModel
+
+@property (copy, nonatomic, nonnull, readonly) NSString *email;
+
+- (instancetype)init __unavailable;
++ (instancetype)new __unavailable;
+
+- (instancetype)initWithEmail:(NSString *)email NS_SWIFT_NAME(init(email:));
+
+@end
+
+NS_ASSUME_NONNULL_END
